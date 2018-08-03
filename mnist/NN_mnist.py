cb648ff7ef63d22505ac7bbf016a3a37adc0c97f
bitacademy-howl <howl1118@gmail.com>
2018-08-03 20:40:50 +0900

    11

---
+++ mnist/NN_mnist.py
@@ -0,0 +1,54 @@
+import tensorflow as tf
+import random
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+learning_rate = 0.001
+training_epochs = 15
+batch_size = 100
+nb_classes = 10
+layer_colsize = 256
+
+X = tf.placeholder(tf.float32, [None, 784])
+Y = tf.placeholder(tf.float32, [None, nb_classes])
+
+W1 = tf.Variable(tf.random_normal([784, layer_colsize]))
+b1 = tf.Variable(tf.random_normal([layer_colsize]))
+L1 = tf.nn.relu(tf.matmul(X, W1) + b1)
+
+W2 = tf.Variable(tf.random_normal([layer_colsize, layer_colsize]))
+b2 = tf.Variable(tf.random_normal([layer_colsize]))
+L2 = tf.nn.relu(tf.matmul(L1, W2) + b2)
+
+W3 = tf.Variable(tf.random_normal([layer_colsize, nb_classes]))
+b3 = tf.Variable(tf.random_normal([nb_classes]))
+hypothesis = tf.matmul(L2, W3) + b3
+
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=hypothesis, labels=Y))
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+
+for epoch in range(training_epochs):
+    avg_cost = 0
+    total_batch = int(mnist.train.num_examples / batch_size)
+    for i in range(total_batch):
+        batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+        feed_dict = {X:batch_xs, Y:batch_ys}
+        c, _ = sess.run([cost, optimizer], feed_dict=feed_dict)
+        avg_cost += c / total_batch
+
+    print('Epoch : ', '%04d' % (epoch + 1), 'cost =', '{:,.9f}'.format(avg_cost))
+print('Learning Finished!')
+
+# Test model and check accuracy
+correct_prediction = tf.equal(tf.argmax(hypothesis, 1), tf.argmax(Y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+print('Accuracy : ', sess.run(accuracy, feed_dict={X:mnist.test.images, Y:mnist.test.labels}))
+
+r = random.randint(0, mnist.test.num_examples - 1)
+print("Label : ", sess.run(tf.argmax(mnist.test.labels[r:r + 1], 1)))
+print("Prediction : ", sess.run(tf.argmax(hypothesis, 1), feed_dict={X:mnist.test.images[r:r + 1]}))
+
